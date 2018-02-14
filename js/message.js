5125b87a5a91a0b22775be8c0255039634ecfb61
luoxin <goodmanluoxin@163.com>
2018-02-14 19:22:40 +0800

    做成vc模式，下一步添加model

--- js/message.js
+++ js/message.js
@@ -1,44 +1,68 @@
-var APP_ID = 'EXFsEg9amld9Kk0AXfkyK7dy-gzGzoHsz';
-var APP_KEY = 'wkF1VU1pLVXIloaa2znryX52';
+!function(){
+    var view = document.querySelector('section.message')
 
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
+    var controller = {
+        view: null,
+        messageList: null,
+        form: null,
+        loadHistory: function(){
+            var query = new AV.Query('ChadDavid');
+            query.find()
+                .then( (todo) => {
+                todo.forEach((item) => {
+                    var messageList = document.getElementById('messageList')
+                    var li = document.createElement('li')
+                    var attributes = item.attributes
+                    li.innerText = attributes.name+' : '+attributes.content
+                    this.messageList.appendChild(li)
+                })
+                }, function (error) {
+                    console.log('留言失败！')
+                })
+        },
 
-var query = new AV.Query('ChadDavid');
-query.find()
-    .then(function (todo) {
-    todo.forEach((item) => {
-        var messageList = document.getElementById('messageList')
-        var li = document.createElement('li')
-        var attributes = item.attributes
-        li.innerText = attributes.name+' : '+attributes.content
-        messageList.appendChild(li)
-    })
-    }, function (error) {
-        console.log('留言失败！')
-    })
+        initAV: function(){
+            var APP_ID = 'EXFsEg9amld9Kk0AXfkyK7dy-gzGzoHsz';
+            var APP_KEY = 'wkF1VU1pLVXIloaa2znryX52';
+            AV.init({appId: APP_ID,appKey: APP_KEY});   
+        },
 
+        bindEvents: function(){
+            this.form.addEventListener('submit', function(e){
+                e.preventDefault()
+                this.saveMess()
+            }.bind(this))
+        },
 
+        saveMess: function(){
+            let content = this.form.querySelector('input[name=content]').value
+            let name = this.form.querySelector('input[name=name]').value
+            var TestObject = AV.Object.extend('ChadDavid');
+            var testObject = new TestObject();
+            testObject.save({
+                content: content,
+                name: name
+              }).then(function(object) {
+                var messageList = document.getElementById('messageList')
+                var li = document.createElement('li')
+                li.innerText = name+' : '+content
+                messageList.appendChild(li)
+              }).then(function(){
+                document.querySelector('input[name=content]').value = ''
+              })
+        },
 
-var form = document.getElementById("postMessage")
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-    let content = document.querySelector('input[name=content]').value
-    let name = document.querySelector('input[name=name]').value
-    var TestObject = AV.Object.extend('ChadDavid');
-    var testObject = new TestObject();
-    testObject.save({
-        content: content,
-        name: name
-      }).then(function(object) {
-        var messageList = document.getElementById('messageList')
-        var li = document.createElement('li')
-        li.innerText = name+' : '+content
-        messageList.appendChild(li)
-      }).then(function(){
-          document.querySelector('input[name=content]').value = ''
-      })
-})
+        init: function(view){
+            this.view = view
+            this.messageList = view.querySelector('#messageList')
+            this.form = view.querySelector('#postMessage')
+            this.initAV()
+            this.loadHistory()
+            this.bindEvents()
+        }
+    }
+
+    controller.init.call(controller, view)
+
+}.call()
 
